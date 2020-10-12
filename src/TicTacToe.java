12a55e40ddd5ad9551da229b50eee1cb0aea07b5
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-12 17:46:24 -0500

    feat: array for x placement

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -17,6 +17,37 @@ public class TicTacToe {
         int position = scan.nextInt();
 
         System.out.println(position);
+
+        switch(position){
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
+        printGameBoard(gameBoard);
     }
         public static void printGameBoard(char[][] gameBoard) {
             for (char[] row : gameBoard) {
